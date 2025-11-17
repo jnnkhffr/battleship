48ca02ee4616911ba3bb591b5978363b89b66f64
Davide <herrdavide@live.de>
2025-11-17 18:38:26 +0100

    own sub classes for each ship type. Size is in config

--- battleship_game/config.py
+++ battleship_game/config.py
@@ -4,3 +4,8 @@ GRID_ROWS = 10
 BLOCK_SIZE = 40  # size of a grid cell
 COLOR_BG: tuple[int, int, int] = (0, 0, 0)
 COLOR_GRID: tuple[int, int, int] = (40, 40, 40)
+
+SUB_SIZE = 1
+FRIG_SIZE = 2
+DEST_SIZE = 3
+ACC_SIZE = 4
--- battleship_game/fleet.py
+++ battleship_game/fleet.py
@@ -1,11 +1,29 @@
+from battleship_game.config import SUB_SIZE, FRIG_SIZE, DEST_SIZE, ACC_SIZE
+
+
 class ship:
-    def __init__(self, size: int, type: str, postion: tuple, orientation: str):
+    def __init__(self, size: int, position: tuple, orientation: str):
         self.size = size
-        self.type = type
-        self.postion = postion
+        self.position = position
         self.orientation = orientation
 
 # All ships in different classes
-submarine = ship(1, 'sub', (1,1), 'horizontal')
-frigate = ship(2,'fri', (,),  )
 
+class submarine(ship):
+    def __init__(self, size: int, position: tuple, orientation: str):
+        super().__init__(size, position, orientation)
+        self.size = SUB_SIZE
+
+class frigate(ship):
+    def __init__(self, size: int, position: tuple, orientation: str):
+        super().__init__(size, position, orientation)
+        self.size = FRIG_SIZE
+
+class destroyer(ship):
+    def __init__(self, size: int, position: tuple, orientation: str):
+        super().__init__(size, position, orientation)
+        self.size = DEST_SIZE
+class aircraft_carrier(ship):
+    def __init__(self, size: int, position: tuple, orientation: str):
+        super().__init__(size, position, orientation)
+        self.size = ACC_SIZE
