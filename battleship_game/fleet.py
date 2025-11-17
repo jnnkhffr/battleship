9266aa559e2ead72f966cdad75851f943d131cb6
Davide <herrdavide@live.de>
2025-11-17 18:19:43 +0100

    added orientation attribut for ship class

--- battleship_game/fleet.py
+++ battleship_game/fleet.py
@@ -1,8 +1,9 @@
 class ship:
-    def __init__(self, size: int, name: str, postion: tuple):
+    def __init__(self, size: int, type: str, postion: tuple, orientation: str):
         self.size = size
-        self.name = name
+        self.type = type
         self.postion = postion
+        self.orientation = orientation
 
 
 submarine = ship(1, 'sub', (1,1))
