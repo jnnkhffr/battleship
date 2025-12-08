57c68903f6580beb3058e34827855393cdc0c157
Davide <herrdavide@live.de>
2025-12-08 18:29:19 +0100

    added color gray for the ships

--- battleship_game/config.py
+++ battleship_game/config.py
@@ -4,6 +4,7 @@ GRID_ROWS = 10
 BLOCK_SIZE = 40  # size of a grid cell
 COLOR_BG: tuple[int, int, int] = (0, 0, 0)
 COLOR_GRID: tuple[int, int, int] = (40, 40, 40)
+COLOR_SHIP: tuple[int, int, int] = (169, 169, 169) # gray
 
 SUB_SIZE = 1
 FRIG_SIZE = 2
