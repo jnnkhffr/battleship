886e5b65013a9bff128f1c513393afa88a8bc9ff
jnnkhffr <jannik.hoeffer@gmx.de>
2025-12-20 13:05:13 +0100

    The board needs information about where the ships are located. This structure is transferred and added to the board.

--- battleship_game/board.py
+++ battleship_game/board.py
@@ -29,6 +29,8 @@ class Board:
         self.bgcolor = bgcolor
         self.gridcolor = gridcolor
 
+        self.grid = [[0 for _ in range(cols)] for _ in range(rows)]
+
     def draw(self, surface: pygame.Surface, offset_x: int = 0) -> None:
         """
         Draws the grid on a given Pygame surface.
@@ -51,3 +53,42 @@ class Board:
             )
             pygame.draw.rect(surface, self.gridcolor, rect, 1)
 
+            # draw ship
+            if self.grid[y][x] == 1:
+                pygame.draw.rect(surface, (169, 169, 169), rect)
+
+
+    def can_place_ship(self, x: int, y: int, size: int, orientation: str) -> bool:
+                """Checks if you can place a ship with one grid between ships)."""
+
+                dx = 1 if orientation == "hor" else 0
+                dy = 1 if orientation == "ver" else 0
+
+                # 1) Ship hast to be within the gamefield
+                for i in range(size):
+                    nx = x + dx * i
+                    ny = y + dy * i
+                    if not (0 <= nx < self.cols and 0 <= ny < self.rows):
+                        return False
+
+                # 2) Checks the distance
+                for i in range(-1, size + 1):
+                    for ox in (-1, 0, 1):
+                        for oy in (-1, 0, 1):
+                            nx = x + dx * i + ox
+                            ny = y + dy * i + oy
+                            if 0 <= nx < self.cols and 0 <= ny < self.rows:
+                                if self.grid[ny][nx] == 1:
+                                    return False
+
+                return True
+
+    def place_ship(self, x: int, y: int, size: int, orientation: str) -> None:
+                """Enters the ship into the grid"""
+                dx = 1 if orientation == "hor" else 0
+                dy = 1 if orientation == "ver" else 0
+
+                for i in range(size):
+                    nx = x + dx * i
+                    ny = y + dy * i
+                    self.grid[ny][nx] = 1
