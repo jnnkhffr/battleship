9275038869e52a6a549dc9317023a25722da64a3
tmgdarshan <darshantamang@gmail.com>
2025-12-24 01:53:48 +0100

    Added the fleet list and ship names with their respective sizes

--- battleship_game/config.py
+++ battleship_game/config.py
@@ -1,29 +1,44 @@
 
-#Board Conifg.
+# Screen and Grid Setting
 GRID_COLS = 10
 GRID_ROWS = 10
-
-BLOCK_SIZE = 40  # size of a grid cell
-
-BOARD_SPACING = BLOCK_SIZE * 2 #distance between two boards
+BLOCK_SIZE = 40  # size of a grid cell in pixels
+BOARD_SPACING = BLOCK_SIZE * 2 #distance between player and computer boards
 
 #Colors
-COLOR_BG: tuple[int, int, int] = (0, 0, 0)
-COLOR_GRID: tuple[int, int, int] = (40, 40, 40)
-COLOR_SHIP: tuple[int, int, int] = (169, 169, 169)
+COLOR_BG = (0, 0, 0)
+COLOR_GRID = (40, 40, 40)
+COLOR_TEXT = (255, 255, 255)
+COLOR_SHIP = (169, 169, 169)
+COLOR_MISS = (0, 150, 255)   # Blue for Miss
+COLOR_HIT  = (255, 165, 0) # Orange for Hit
+COLOR_SUNK = (200, 0, 0)  # Red for sunk
 
-#Ship config.
-SUB_SIZE = 1
-FRIG_SIZE = 2
-DEST_SIZE = 3
-ACC_SIZE = 4
+# Ship config
+SHIP_SIZES = {
+    "Submarine": 1,
+    "Frigate": 2,
+    "Destroyer": 3,
+    "Aircraft Carrier": 4
+}
 
+
+FLEET = [
+    "Aircraft Carrier",
+    "Destroyer",
+    "Frigate",
+    "Submarine",
+    "Submarine",
+    "Submarine",
+    "Submarine"
+]
 NUM_SUBS = 4
 NUM_FRIGS = 1
 NUM_DESTS = 1
 NUM_ACCS = 1
 
-#GAmeplay Settings
+# Gameplay Settings
 DEFAULT_ORIENTATION = "hor"
 SHIP_MARGIN = 1 #distance between ships
 DEBUG_SHOW_ENEMY_SHIPS = True # or False
+WINDOW_TITLE = "BATTLESHIP"
